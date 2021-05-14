4e5e8ffaa8f6f034dd05cdc3b9eac2dd240038a2
steinko <steinkor@gmail.com>
2021-05-14 22:18:22 +0200

    first commit

---
+++ src/test/java/org/steinko/serverless/ServerlessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.steinko.serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
